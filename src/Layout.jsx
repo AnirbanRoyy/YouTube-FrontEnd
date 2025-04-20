255552f7aa9663d181a26cce1c87df972ccfe097
Anirban Roy <anirban.roy@bluevector.ai>
2025-04-20 13:35:24 +0530

    Refactor Layout component to manage sidebar state and improve layout structure

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
+    const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+
+    const handleSidebarToggle = (collapsed) => {
+        setIsSidebarCollapsed(collapsed);
+    };
+
     return (
         <>
             <NavBar />
-            <Outlet />
-            <SideBar />
+            <div className="flex">
+                <SideBar onToggle={handleSidebarToggle} />
+                <div
+                    className={`flex-1 transition-all duration-75 bg-gray-900 ${
+                        isSidebarCollapsed ? "ml-20" : "ml-48"
+                    }`}
+                >
+                    <Outlet />
+                </div>
+            </div>
         </>
     );
 }
